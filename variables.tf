4d75bcf71c3987c5188e54f56bada1869e96fefd
Leandro Scardua <leandroscar@gmail.com>
2022-04-26 06:15:18 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,13 +7,3 @@ variable "global_name_app" {
     type    = string
     default = ""
 }
-
-variable "appservice_ip_block" {  
-    type = string 
-    default = "0.0.0.0/0"
-}
-
-variable "appservice_ip_allow" {  
-    type = string 
-    default = "xxx.xxx.xxx.xxx/xx"
-}
